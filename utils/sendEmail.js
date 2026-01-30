099684cf67f5c2fe92d3ea320485463ce89344ac
prashantnigam1994 <prashantnigam094@gmail.com>
2026-01-30 15:13:22 +0530

    iNotebbok backend code

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,10 +2,11 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.sendgrid.net",
+        port: 587,
         auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: "apikey",
+            pass: process.env.SENDGRID_API_KEY,
         },
     });
 
