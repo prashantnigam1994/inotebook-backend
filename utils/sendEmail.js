4b9d25d7989349316516d42f7561eb59430202f2
prashantnigam1994 <prashantnigam094@gmail.com>
2026-01-30 15:46:00 +0530

    iNotebbok backend code

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,4 +1,5 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
@@ -11,7 +12,7 @@ const sendEmail = async (options) => {
     });
 
     const mailOptions = {
-        from: `"iNotebook Support" <${process.env.EMAIL_USER}>`,
+        from: `"iNotebook Support" <prashantnigam094@gmail.com>`,
         to: options.email,
         subject: options.subject,
         text: options.message,
